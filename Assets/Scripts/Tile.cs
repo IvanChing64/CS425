f4610589f1afeb2e08ac29469597456236c786ee
IvanChingUNR <chingnavi88@gmail.com>
2025-10-26 14:17:02 -0700

    code to make a grid system.
    Code that makes grid tiles on start and allows users to hover the tiles to highlight which tiles the mouse is on.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color baseColor, offsetColor;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject highlight;
+
+    public void Init(bool isOffset)
+    {
+        Debug.Log($"{name} Init() called. Renderer is {_renderer}");
+        _renderer.color = isOffset ? offsetColor : baseColor;
+    }
+
+    void OnMouseEnter()
+    {
+        highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        highlight.SetActive(false);
+    }
+}
