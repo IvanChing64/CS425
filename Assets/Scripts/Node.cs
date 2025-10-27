a5db8df3ae24d9b70ae68e0bc58952e0218da6c0
LoudNoYou <andrew.1222084@gmail.com>
2025-10-26 22:21:37 -0700

    Pathfinding Start
    Have implemented the beginning of the pathfinding code.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node: MonoBehaviour
+{
+    public Node cameFrom;
+    public List<Node> connections;
+
+    public float gScore;
+    public float hScore;
+
+    public float Fscore()
+    {
+        return gScore + hScore;    
+    }
+    
+}
+
