3cd02efd7e60c0799ef041ae4158634bcc17eb5b
Bailey Escritor <hawaiiicedog@gmail.com>
2026-01-05 17:33:02 -0800

    Current Progress
    Implementing some of the new card related managers and some of the implementation of card prefabs

---
+++ Assets/Scripts/Managers/DeckManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+//Developer: Bailey Escritor
+//Handles the creation and manipulation of all player decks
+public class DeckManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
