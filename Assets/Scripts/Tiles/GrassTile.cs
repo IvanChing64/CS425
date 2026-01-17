06be9cccacdcf1375ad9b9615b3a17a81b0bfe34
IvanChingUNR <chingnavi88@gmail.com>
2026-01-17 15:46:18 -0800

    tile changes

--- Assets/Scripts/Tiles/GrassTile.cs
+++ Assets/Scripts/Tiles/GrassTile.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class GrassTile : Tile
 {
     [SerializeField] private Color baseColor, offsetColor;
+    [SerializeField] private Sprite[] grassSprites;
 
     //Checkerboard Pattern
     public override void Init(int x, int y)
     {
+
+        if (grassSprites != null && grassSprites.Length > 0)
+        {
+            _renderer.sprite = grassSprites[Random.Range(0, grassSprites.Length)];
+        }
         var isOffset = (x + y) % 2 == 1;
         Debug.Log($"{name} Init() called. Renderer is {_renderer}");
         _renderer.color = isOffset ? offsetColor : baseColor;
