d8d2bec9feaf58d5e9d6bba291e1ab168e4be9f3
LoudNoYou <andrew.1222084@gmail.com>
2026-04-04 17:01:53 -0700

    Just changed where the flag system is
    Attack Range works but I need to add movement behavior for Ranged enemies. Not done yet

--- Assets/Scripts/Units/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Units/Enemy/BaseEnemy.cs
@@ -2,25 +2,7 @@ using UnityEngine;
 
 public class BaseEnemy : BaseUnit
 {
-    //New stuff: Enemy Flags - Andrew Shelton
-    public EnemyFlag enemyFlag;
 
-    public override void ApplyFlags()
-    {
-        switch (enemyFlag)
-        {
-            case EnemyFlag.Ranged:
-                attackRange += 3;
-                maxHealth -= 95;
-                break;
-
-            case EnemyFlag.Support:
-                attackBoost += 0.5f;
-                defenseBoost += 0.5f;
-                break;
-               
-        }
-    }
     public override void SelectUnit()
     {
 
--- Assets/Scripts/Units/Enemy/Enemy1.cs
+++ Assets/Scripts/Units/Enemy/Enemy1.cs
@@ -2,9 +2,29 @@ using UnityEngine;
 
 public class Enemy1 : BaseEnemy
 {
+    //New stuff: Enemy Flags - Andrew Shelton
+    public EnemyFlag enemyFlag;
+
+    public override void ApplyFlags()
+    {
+        switch (enemyFlag)
+        {
+            case EnemyFlag.Ranged:
+                attackRange += 3;
+                maxHealth -= 95;
+                break;
+
+            case EnemyFlag.Support:
+                attackBoost += 0.5f;
+                defenseBoost += 0.5f;
+                break;
+
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        ApplyFlags();
         
     }
 
