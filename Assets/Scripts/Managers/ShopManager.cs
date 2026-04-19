d3ca7a21ca2fe38359d5c8d55e28e7b5ec8402ae
Liam Riel <shyguy1580@gmail.com>
2026-04-18 20:47:07 -0700

    Shop screen updates

--- Assets/Scripts/Managers/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,17 +6,44 @@ public class ShopManager : MonoBehaviour
 {
     public static ShopManager Instance;
 
-    public List<ScriptableItem> itemsInShop;
+    public static List<ScriptableItem> ItemsInShop;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         Instance = this;
-        GenerateShop();
     }
 
-    void GenerateShop()
+    bool BuyItem(ScriptableItem item)
     {
-        
+        bool purchased = ArmyManager.Instance.AttemptPurchase(item.cost);
+        if (!purchased) return false;
+
+        switch (item.type)
+        {
+            case ItemType.UnitUpgrade:
+                break;
+            case ItemType.NewUnit:
+                break;
+            case ItemType.PartyBuff:
+                break;
+            case ItemType.DeckAddition:
+                break;
+            default:
+                Debug.Log("Invalid shop item type");
+                break;
+        }
+
+        return true;
+    }
+
+    void RestockShop()
+    {
+
+    }
+
+    void RestockItemIn(int slot)
+    {
+        ItemsInShop[slot] = null;
     }
 }
