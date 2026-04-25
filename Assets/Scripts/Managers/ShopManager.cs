d7d2cfc7b8c5d78a7abf515dfbc8772de90a29da
Liam Riel <shyguy1580@gmail.com>
2026-04-25 14:10:13 -0700

    Shop rerolls and comments

--- Assets/Scripts/Managers/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
+/// <summary>
+/// A class for handling the shop and purchasing items
+/// </summary>
+/// <remarks>Primary author: Liam Riel</remarks>
 public class ShopManager : MonoBehaviour
 {
     public static ShopManager Instance;
@@ -50,6 +54,9 @@ public class ShopManager : MonoBehaviour
         UpdateRerollCostText();
     }
 
+    /// <summary>
+    /// Used when the shop has not yet stocked any items
+    /// </summary>
     private void InitializeStock()
     {
         CurrentItemsInShop = new List<ScriptableItem>(itemSlots.Count);
@@ -62,6 +69,10 @@ public class ShopManager : MonoBehaviour
         RestockShop();
     }
 
+    /// <summary>
+    /// Called when the attempts to buy the item in an item slot
+    /// </summary>
+    /// <param name="slot">The item slot to buy the item from</param>
     public void BuyItemInSlot(int slot)
     {
         if (!BuyItem(CurrentItemsInShop[slot])) return;
@@ -70,6 +81,11 @@ public class ShopManager : MonoBehaviour
         UpdateItemSlot(slot);
     }
 
+    /// <summary>
+    /// Checks if the player has enough money to buy an item and if so, buys it and applies its effect
+    /// </summary>
+    /// <param name="item">The item to buy</param>
+    /// <returns>Whether the item was bought</returns>
     public bool BuyItem(ScriptableItem item)
     {
         bool purchased = ArmyManager.Instance.AttemptPurchase(item.cost);
@@ -109,6 +125,10 @@ public class ShopManager : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// Restocks/rerolls the statically stored shop items
+    /// </summary>
+    /// <remarks>Does not update the item slots shown in the shop</remarks>
     public void RestockShop()
     {
         for (int i = 0; i < itemSlots.Count; i++)
@@ -117,6 +137,10 @@ public class ShopManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Restocks/rerolls one of the statically stored shop items
+    /// </summary>
+    /// <param name="slot">The item slot/index to restock</param>
     public void RestockItemIn(int slot)
     {
         ScriptableItem itemToAdd;
@@ -138,6 +162,10 @@ public class ShopManager : MonoBehaviour
         CurrentItemsInShop[slot] = itemToAdd;
     }
 
+    /// <summary>
+    /// Finds and returns a possible unit upgrade item the player can use
+    /// </summary>
+    /// <returns>A random unit upgrade of possible ones the player can obtain, null if there are none</returns>
     private UnitUpgradeItem GetApplicableUpgradeItem()
     {
         List<UnitUpgradeItem> applicableList = new(UnitUpgradeItems.Where(item => ArmyManager.Instance.HasUnit(item.originalUnit) && !CurrentItemsInShop.Contains(item)));
@@ -147,6 +175,9 @@ public class ShopManager : MonoBehaviour
         return applicableList[Random.Range(0, applicableList.Count)];
     }
 
+    /// <summary>
+    /// The reroll button uses this function to attempt to reroll the shop and update the reroll cost
+    /// </summary>
     public void RerollButton()
     {
         int rerollCost = (int)Math.Round(rerollBaseCost * RerollCostMultiplier);
@@ -156,15 +187,22 @@ public class ShopManager : MonoBehaviour
         RerollShop();
         RerollCostMultiplier += 0.5f;
 
+        UpdateCurrencyText();
         UpdateRerollCostText();
     }
 
+    /// <summary>
+    /// Restocks/rerolls the static shop items and updates the shop scene's slots to match
+    /// </summary>
     public void RerollShop()
     {
         RestockShop();
         UpdateItemSlots();
     }
 
+    /// <summary>
+    /// Updates the visual item slots in the shop scene to match the internal shop stock
+    /// </summary>
     public void UpdateItemSlots()
     {
         for (int i = 0; i < itemSlots.Count; i++)
@@ -173,6 +211,10 @@ public class ShopManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Updates the specified item slot in the shop scene to reflect the static shop stock
+    /// </summary>
+    /// <param name="slot"></param>
     public void UpdateItemSlot(int slot)
     {
         itemSlots[slot].itemImage.sprite = CurrentItemsInShop[slot].itemSprite;
@@ -197,23 +239,29 @@ public class ShopManager : MonoBehaviour
         itemSlots[slot].costText.text = $"Cost: {CurrentItemsInShop[slot].cost}";
     }
 
+    /// <summary>
+    /// Updates the currency text with the player's current gold
+    /// </summary>
     public void UpdateCurrencyText()
     {
         currencyText.text = $"Gold: {ArmyManager.Instance.GetCurrency()}";
     }
 
+    /// <summary>
+    /// Updates the player's army capacity text with the player's army size, and turns it red if it's full
+    /// </summary>
     public void UpdateArmyCapText()
     {
         int armyCount = ArmyManager.Instance.unitsInArmy.Count;
         int armyCap = ArmyManager.Instance.ArmyCapacity;
         armyCapText.text = $"{armyCount} / {armyCap} units";
 
-        if (armyCount >= armyCap)
-        {
-            armyCapText.color = Color.softRed;
-        }
+        armyCapText.color = (armyCount >= armyCap)? Color.softRed : Color.whiteSmoke;
     }
 
+    /// <summary>
+    /// Updates the text for the cost of rerolls
+    /// </summary>
     public void UpdateRerollCostText()
     {
         rerollCostText.text = $"Cost: {(int)Math.Round(rerollBaseCost * RerollCostMultiplier)}";
